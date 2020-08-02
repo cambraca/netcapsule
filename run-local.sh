563e14d70468840e3ce0ee540273d4833e869b7b
Camilo Bravo <hola@cambraca.com>
2020-08-01 20:01:43 -0500

    --x-networking flag not supported (or required) anymore

--- run-local.sh
+++ run-local.sh
@@ -2,5 +2,5 @@
 export ARCHIVE_JSON=./archives.gen.json
 export RANDOM_URL_LIST=./urls.txt
 python -c "import yaml; import json; data = yaml.load(open('archives.yaml')); open('$ARCHIVE_JSON', 'w').write(json.dumps(data))"
-docker-compose --x-networking build
-docker-compose --x-networking up -d
+docker-compose build
+docker-compose up -d
